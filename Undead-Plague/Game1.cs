61301be3b440a518b21b180dff8d06bc6f115047
eeeENVI <Skyficjusz@gmail.com>
2024-03-09 20:28:35 +0100

    Game1.cs Starting point refactoring, making life simpler

--- Undead-Plague/Game1.cs
+++ Undead-Plague/Game1.cs
@@ -1,51 +1,82 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
-namespace Undead_Plague;
+using States;
 
+namespace ProjectGui;
+
+// Only 1 class should inherit from Game
 public class Game1 : Game
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
+    private Stack<State> states;
 
     public Game1()
     {
-        _graphics = new GraphicsDeviceManager(this);
+        Globals.Graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+    }
+
+    private void InitStates()
+    {
+        states = new Stack<State>();
+        states.Push(new TestState());
     }
 
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        InitStates();
 
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        Globals.GraphicsDevice = GraphicsDevice;
+        Globals.SpriteBatch = new SpriteBatch(Globals.GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        if(states.Count != 0)
+        {  
+            states.Peek().LoadContent();
+        }
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
-
         // TODO: Add your update logic here
+        if(states.Count != 0)
+        {
+            states.Peek().Update(gameTime);     
+            
+            if(states.Peek().quit)
+            {
+                //dispose of state data inside End i guess
+                states.Peek().End();
+                states.Pop();
+            }
+        }
+        else 
+        {
+            Exit();
+        }
 
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-
         // TODO: Add your drawing code here
-
+        if(base.IsActive)
+        {
+            //GraphicsDevice.Clear(Color.Black);
+            if(states.Count != 0)
+            {
+                states.Peek().Draw();
+            }
+        }
         base.Draw(gameTime);
     }
 }
